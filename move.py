80bd681c1aa65ed610f0731501cd4528c36cee7b
Edward Agombar <ejagombar@gmail.com>
2023-11-04 23:44:57 +0000

    add more startup

--- move.py
+++ move.py
@@ -8,10 +8,13 @@ p = printcore('/dev/ttyUSB0', 115200)
 # gcode = gcoder.LightGCode(gcode)
 
 # startprint silently exits if not connected yet print("Connecting...")
+XMAX = 40
+YMAX = 35
 
 
 def kickFront():
     p.send("G0 E00")
+    p.send("G0 E01")
 
 
 print("Connecting...")
@@ -27,16 +30,26 @@ print("Turning off Fans")
 # p.send_now("M105")
 
 p.send("M502")
-p.send("G28")
+p.send("G28 X Y")
 p.send("M204 T5000 P5000 R5000 S5000")
 p.send("M203 E5000 Z5000 X5000 Y5000")
 p.send("M201 E5000 Z5000 X5000 Y5000")
-p.send("M92 X180 Y180 E300 Z300")
-p.send("M220 300")
-p.send("G0 E00")
-p.send("G0 X30")
-p.send("G0 Y30")
-p.send("G0 Z30")
+p.send("M92 X180 Y180 E300 Z200")
+p.send("M220 S200")
+
+p.send("G0 Z0")
+p.send("G0 E0")
+
+p.send("G0 X"+str(XMAX//2))
+p.send("G0 Y"+str(YMAX//2))
+
+print("Disable Stepper Motors. Set Position!")
+p.send("M18")
+time.sleep(3)
+print("Renabling Soon...")
+time.sleep(1)
+p.send("M17")
+print("Stepper Motors Re-enabled")
 
 
 p.pause()  # use these to pause/resume the current print
